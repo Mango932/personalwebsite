1b14c37b068fd69b0bfcf96df52ebe111607ebe5
Sam <94940036+Mango932@users.noreply.github.com>
2024-01-27 14:53:46 -0500

    Added vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import Main from "./components/Main";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-    return <Main />;
+    return (
+        <Main>
+            <Analytics />
+        </Main>
+    );
 }
 
 export default App;
